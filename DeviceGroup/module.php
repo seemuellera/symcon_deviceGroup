ab3cfdb9279d31f910fa428c04970a5f3fce4b7a
Andreas Seemueller <andreas.seemueller@gmail.com>
2023-08-03 22:08:28 +0200

    sdf

--- DeviceGroup/module.php
+++ DeviceGroup/module.php
@@ -69,9 +69,12 @@ class DeviceGroup extends IPSModule {
 
 		// Clean old message registration
 		$messagesList = $this->GetMessageList();
-		foreach ($messagesList as $currentMessage) {
+		foreach ($messagesList as $currentMessageVarId => $currentMessageIDs) {
 
-			$this->UnregisterMessage($currentMessage, VM_UPDATE);
+			foreach ($currentMessageIDs as $currentMessageID) {
+
+				$this->UnregisterMessage($currentMessageVarId, $currentMessageID);
+			}
 		}
 
 		// Register Variables if applicable
